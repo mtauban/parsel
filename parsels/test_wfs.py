7da6c7692075cdf81cb11c20abbed8ee6404b6e9
mtauban <mathieu.tauban@gmail.com>
2021-02-07 18:12:50 +0100

    tests for the ign interaction

--- parsels/test_wfs.py
+++ parsels/test_wfs.py
@@ -2,22 +2,46 @@ from owslib.wfs import WebFeatureService
 import json
 
 
-apikey = "7tbcsy3xj9ymeoi4mjdlyayo"
+ign_apikey = "7tbcsy3xj9ymeoi4mjdlyayo"
 # apikey = "beta"
 
-wfs11 = WebFeatureService(url='https://wxs.ign.fr/'+apikey+'/geoportail/wfs', version='1.1.0', headers={ 'User-Agent': 'parcelle-recs' })
+wfs11 = WebFeatureService(url='https://wxs.ign.fr/'+ign_apikey+'/geoportail/wfs', version='2.0.0', headers={ 'User-Agent': 'parcelle-recs' })
 
 print('ok')
 print(wfs11.identification.title)
 print([operation.name for operation in wfs11.operations])
+print([storedquery.id for storedquery in wfs11.storedqueries])
 print(list(wfs11.contents))
 
 print(wfs11.headers)
-response = wfs11.getfeature(typename='CADASTRALPARCELS.PARCELLAIRE_EXPRESS:parcelle', bbox=(4.8363447189331055,45.75274517163593,4.8415718623437,45.75283716884888),outputFormat='application/json')
-
-t = (response.read())
-print(t)
-a = json.dumps(json.loads(t))
 
 
-print(a)
+mf = 500
+
+totalResults = mf
+loadedResults = 0
+
+globalfeatures = None
+while (loadedResults<totalResults):
+
+    response = wfs11.getfeature(typename='CADASTRALPARCELS.PARCELLAIRE_EXPRESS:parcelle',
+                            bbox=(4.8363447189331055,45.75274517163593,4.8415718623437,45.75283716884888),outputFormat='application/json',
+                            maxfeatures=mf,
+                            startindex=loadedResults)
+    t = (response.read())
+    a = json.loads(t)
+
+    #print(json.dumps(a))
+    if (loadedResults == 0):
+        totalResults = a["totalFeatures"]
+        globalfeatures = a
+    else:
+        print(a)
+        globalfeatures["features"] = globalfeatures["features"] + a["features"]
+
+    loadedResults += a["numberReturned"]
+    #print(t)
+print(totalResults,loadedResults)
+# a = json.dumps(json.loads(t))
+
+# print(a)
---
+++ parsels/test_wfs_getfeaturebyid.py
@@ -0,0 +1,28 @@
+from owslib.wfs import WebFeatureService
+import json
+
+
+ign_apikey = "7tbcsy3xj9ymeoi4mjdlyayo"
+# apikey = "beta"
+
+wfs11 = WebFeatureService(url='https://wxs.ign.fr/'+ign_apikey+'/geoportail/wfs', version='2.0.0', headers={ 'User-Agent': 'parcelle-recs' })
+
+print('ok')
+print(wfs11.identification.title)
+print([operation.name for operation in wfs11.operations])
+print([storedquery.id for storedquery in wfs11.storedqueries])
+print(list(wfs11.contents))
+
+print(wfs11.headers)
+
+
+mf = 500
+
+totalResults = mf
+loadedResults = 0
+
+globalfeatures = None
+
+response = wfs11.getfeature(typename='CADASTRALPARCELS.PARCELLAIRE_EXPRESS:parcelle',outputFormat='application/json', featureid=['parcelle.75964254'])
+t = (response.read())
+print(t)
